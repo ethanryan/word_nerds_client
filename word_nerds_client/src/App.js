81bd8dec838d950264cf4a0022d0cf94ac1b2f04
ethanryan <ethanryan@gmail.com>
2017-06-03 22:20:58 -0400

    created React frontend

---
+++ word_nerds_client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+import NavBar from './components/NavBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h2>Word Nerds</h2>
+        <p className="App-intro">
+          yo yo oyo yo yoy o
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ word_nerds_client/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <h1>NavBar here, Login, Signup, etc.</h1>
+  )
+}
+
+export default NavBar
