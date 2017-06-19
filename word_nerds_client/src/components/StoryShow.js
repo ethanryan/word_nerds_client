781395dc365ff534519af873387c8ed94f1395cd
ethanryan <ethanryan@gmail.com>
2017-06-18 22:49:00 -0400

    added line breaks to StoryShow page content

--- word_nerds_client/src/components/StoryShow.js
+++ word_nerds_client/src/components/StoryShow.js
@@ -48,49 +48,16 @@ const StoryShow = (props) => {
 
               <strong>
                 Story content:
-              </strong> (splitting and adding line breaks)
+              </strong>
 
               <br></br>
               <br></br>
 
-              {/* {paragraph.text} */}
-              {/* {story.paragraphs.map((paragraph, key) => {
-                return (
-                  <span key={key}>
-                    {replaceAll(paragraph.text, "HERO", story.characters[0] ? story.characters[0].name : 'Hero')}
-                    <br></br>
-                    <br></br>
-                  </span>
-                )
-              })} */}
-
-              {/* now doing above, below is the old way: */}
-              {/* old way is better way???: */}
-
-
-              {/* {story.content.split('-----').map((paragraph) => {
-                return (
-                <span key={paragraph.id}>
-                  {paragraph}
-                  <br/><br/>
-                </span>
-              )
-              })} */}
-
-              props.story.content:
               {props.story.content}
 
 
-                props.story.content, mapped over:
-                {props.story.content.split('-----').map((paragraph) => {
-                return (
-                <span key={paragraph.id}>
-                  {paragraph}
-                  <br/><br/>
-                </span>
-              )
-              })}
-
+              <br></br>
+              <br></br>
               Story ID: {props.story.id}
               <br></br>
 
@@ -102,6 +69,13 @@ const StoryShow = (props) => {
                   Delete
                 </button>
               </div>
+
+              Edit this story:
+              <Link className='btn btn-primary'
+                to={`/stories/${props.story.id}/edit`}
+              > {props.story.title}</Link>
+              <br></br>
+              <br></br>
 
           </div>
     </div>
