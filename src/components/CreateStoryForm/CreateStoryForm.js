8f8c920cef9b38ff41fea6fb6c672ed9a3a7af4a
ethanryan <ethanryan@gmail.com>
2018-03-15 08:30:03 -0400

    adding description / instructions to CreateStoryForm

--- src/components/CreateStoryForm/CreateStoryForm.js
+++ src/components/CreateStoryForm/CreateStoryForm.js
@@ -11,77 +11,86 @@ const CreateStoryForm = (props) => {
   return(
     <div className="center modal-trigger-wrapper">
 
-      <div className="modal-trigger-segment">
-        <Segment>
-          Create your story below!
-        </Segment>
-      </div>
+      <Segment>
 
-      <div className="modal-trigger">
-        <Card fluid>
-          <Card.Content>
-            <CreateMiniStoryFormModal
-              handleSubmit={props.handleSubmit}
-              user_id={props.user_id}
-              genreSelection={props.genreSelection}
-              plots={props.plots}
-              handleGenreChange={props.handleGenreChange}
-              //below will be passed down to CreateStoryFormCreateCharacters:
-              //refactor below!!! don't need all these functions, just one or two...
-              characterProps={props.characterProps}
-              handleHeroNameChange={props.handleHeroNameChange}
-              handleShadowNameChange={props.handleShadowNameChange}
-              handleFriendNameChange={props.handleFriendNameChange}
-              handleLoverNameChange={props.handleLoverNameChange}
-              handleMentorNameChange={props.handleMentorNameChange}
-              handleTricksterNameChange={props.handleTricksterNameChange}
-              //refactor below!!! don't need all these functions, just one or two...
-              handleHeroGenderChange={props.handleHeroGenderChange}
-              handleShadowGenderChange={props.handleShadowGenderChange}
-              handleFriendGenderChange={props.handleFriendGenderChange}
-              handleLoverGenderChange={props.handleLoverGenderChange}
-              handleMentorGenderChange={props.handleMentorGenderChange}
-              handleTricksterGenderChange={props.handleTricksterGenderChange}
-            />
-            {/* above will render trigger to button and modal inside trigger... */}
-
-            <Segment>Create a 5 sentence mini story.</Segment>
+        <div className="modal-trigger-description">
+          <Card fluid>
+            <Card.Content>
+            <Segment>
+              Create your story below!
+            </Segment>
           </Card.Content>
-        </Card>
-      </div>
+          </Card>
+        </div>
 
-      <div className="modal-trigger">
-        <Card fluid>
-          <Card.Content>
-            <CreateStoryFormModal
-              handleSubmit={props.handleSubmit}
-              user_id={props.user_id}
-              genreSelection={props.genreSelection}
-              plots={props.plots}
-              handleGenreChange={props.handleGenreChange}
-              //below will be passed down to CreateStoryFormCreateCharacters:
-              //refactor below!!! don't need all these functions, just one or two...
-              characterProps={props.characterProps}
-              handleHeroNameChange={props.handleHeroNameChange}
-              handleShadowNameChange={props.handleShadowNameChange}
-              handleFriendNameChange={props.handleFriendNameChange}
-              handleLoverNameChange={props.handleLoverNameChange}
-              handleMentorNameChange={props.handleMentorNameChange}
-              handleTricksterNameChange={props.handleTricksterNameChange}
-              //refactor below!!! don't need all these functions, just one or two...
-              handleHeroGenderChange={props.handleHeroGenderChange}
-              handleShadowGenderChange={props.handleShadowGenderChange}
-              handleFriendGenderChange={props.handleFriendGenderChange}
-              handleLoverGenderChange={props.handleLoverGenderChange}
-              handleMentorGenderChange={props.handleMentorGenderChange}
-              handleTricksterGenderChange={props.handleTricksterGenderChange}
-            />
-            {/* above will render trigger to button and modal inside trigger... */}
 
-            <Segment>Create a 500-word short story.</Segment>
-          </Card.Content>
-        </Card>
-      </div>
+        <div className="modal-trigger">
+          <Card fluid>
+            <Card.Content>
+              <CreateMiniStoryFormModal
+                handleSubmit={props.handleSubmit}
+                user_id={props.user_id}
+                genreSelection={props.genreSelection}
+                plots={props.plots}
+                handleGenreChange={props.handleGenreChange}
+                //below will be passed down to CreateStoryFormCreateCharacters:
+                //refactor below!!! don't need all these functions, just one or two...
+                characterProps={props.characterProps}
+                handleHeroNameChange={props.handleHeroNameChange}
+                handleShadowNameChange={props.handleShadowNameChange}
+                handleFriendNameChange={props.handleFriendNameChange}
+                handleLoverNameChange={props.handleLoverNameChange}
+                handleMentorNameChange={props.handleMentorNameChange}
+                handleTricksterNameChange={props.handleTricksterNameChange}
+                //refactor below!!! don't need all these functions, just one or two...
+                handleHeroGenderChange={props.handleHeroGenderChange}
+                handleShadowGenderChange={props.handleShadowGenderChange}
+                handleFriendGenderChange={props.handleFriendGenderChange}
+                handleLoverGenderChange={props.handleLoverGenderChange}
+                handleMentorGenderChange={props.handleMentorGenderChange}
+                handleTricksterGenderChange={props.handleTricksterGenderChange}
+              />
+              {/* above will render trigger to button and modal inside trigger... */}
+
+              <Segment>Create a 5 sentence mini story.</Segment>
+            </Card.Content>
+          </Card>
+        </div>
+
+        <div className="modal-trigger">
+          <Card fluid>
+            <Card.Content>
+              <CreateStoryFormModal
+                handleSubmit={props.handleSubmit}
+                user_id={props.user_id}
+                genreSelection={props.genreSelection}
+                plots={props.plots}
+                handleGenreChange={props.handleGenreChange}
+                //below will be passed down to CreateStoryFormCreateCharacters:
+                //refactor below!!! don't need all these functions, just one or two...
+                characterProps={props.characterProps}
+                handleHeroNameChange={props.handleHeroNameChange}
+                handleShadowNameChange={props.handleShadowNameChange}
+                handleFriendNameChange={props.handleFriendNameChange}
+                handleLoverNameChange={props.handleLoverNameChange}
+                handleMentorNameChange={props.handleMentorNameChange}
+                handleTricksterNameChange={props.handleTricksterNameChange}
+                //refactor below!!! don't need all these functions, just one or two...
+                handleHeroGenderChange={props.handleHeroGenderChange}
+                handleShadowGenderChange={props.handleShadowGenderChange}
+                handleFriendGenderChange={props.handleFriendGenderChange}
+                handleLoverGenderChange={props.handleLoverGenderChange}
+                handleMentorGenderChange={props.handleMentorGenderChange}
+                handleTricksterGenderChange={props.handleTricksterGenderChange}
+              />
+              {/* above will render trigger to button and modal inside trigger... */}
+
+              <Segment>Create a 500-word short story.</Segment>
+            </Card.Content>
+          </Card>
+        </div>
+
+      </Segment>
     </div>
   )
 }
