7f3c9f621575ce54ebc305c4d193e6cebe25a2a5
ethanryan <ethanryan@gmail.com>
2018-06-28 11:52:04 -0400

    updating StoryPage formatting

--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -26,150 +26,153 @@ const StoryPage = (props) => {
       <Grid stackable centered columns={2}>
         {/* <Grid.Row> */}
 
-          <Grid.Column width={12}>
+        <Grid.Column width={12}>
 
-            {/* <all the switches within StoryPage, forms, show, index, etc.../> */}
+          {/* <all the switches within StoryPage, forms, show, index, etc.../> */}
 
-            <Switch>
-              <Route exact path='/'
-              render={() => <CreateStoryForm
-                scrollToTop={props.scrollToTop}
-                handleSubmit={props.handleSubmit}
-                handleClearForm={props.handleClearForm}
-                user_id={props.user ? props.user.id : "props.user.id here"}
-                genreSelection={props.genreSelection} //this goes to CreateStoryForm
-                //below will be passed down to CreateStoryFormSelectGenre:
-                plots={props.plots}
-                handleGenreChange={props.handleGenreChange}
-                replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
-                //below will be passed down to CreateStoryFormCreateCharacters:
-                //refactor below!!! don't need all these functions, just one or two...
-                characterProps={props.characterProps}
-                handleHeroNameChange={props.handleHeroNameChange}
-                handleShadowNameChange={props.handleShadowNameChange}
-                handleFriendNameChange={props.handleFriendNameChange}
-                handleLoverNameChange={props.handleLoverNameChange}
-                handleMentorNameChange={props.handleMentorNameChange}
-                handleTricksterNameChange={props.handleTricksterNameChange}
-                //refactor below!!! don't need all these functions, just one or two...
-                handleHeroGenderChange={props.handleHeroGenderChange}
-                handleShadowGenderChange={props.handleShadowGenderChange}
-                handleFriendGenderChange={props.handleFriendGenderChange}
-                handleLoverGenderChange={props.handleLoverGenderChange}
-                handleMentorGenderChange={props.handleMentorGenderChange}
-                handleTricksterGenderChange={props.handleTricksterGenderChange}
-              />} />
+          <Switch>
+            <Route exact path='/'
+            render={() => <CreateStoryForm
+              scrollToTop={props.scrollToTop}
+              handleSubmit={props.handleSubmit}
+              handleClearForm={props.handleClearForm}
+              user_id={props.user ? props.user.id : "props.user.id here"}
+              genreSelection={props.genreSelection} //this goes to CreateStoryForm
+              //below will be passed down to CreateStoryFormSelectGenre:
+              plots={props.plots}
+              handleGenreChange={props.handleGenreChange}
+              replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
+              //below will be passed down to CreateStoryFormCreateCharacters:
+              //refactor below!!! don't need all these functions, just one or two...
+              characterProps={props.characterProps}
+              handleHeroNameChange={props.handleHeroNameChange}
+              handleShadowNameChange={props.handleShadowNameChange}
+              handleFriendNameChange={props.handleFriendNameChange}
+              handleLoverNameChange={props.handleLoverNameChange}
+              handleMentorNameChange={props.handleMentorNameChange}
+              handleTricksterNameChange={props.handleTricksterNameChange}
+              //refactor below!!! don't need all these functions, just one or two...
+              handleHeroGenderChange={props.handleHeroGenderChange}
+              handleShadowGenderChange={props.handleShadowGenderChange}
+              handleFriendGenderChange={props.handleFriendGenderChange}
+              handleLoverGenderChange={props.handleLoverGenderChange}
+              handleMentorGenderChange={props.handleMentorGenderChange}
+              handleTricksterGenderChange={props.handleTricksterGenderChange}
+            />}
+          />
 
-              <Route
-                exact path='/stories/:id'
-                render={({match}) => {
-                  const story = props.stories.find( s => s.id === parseInt(match.params.id, 10))
+          <Route
+            exact path='/stories/:id'
+            render={({match}) => {
+              const story = props.stories.find( s => s.id === parseInt(match.params.id, 10))
+              return
+              <div className="StoryShow-purple">
+                <StoryShow
+                  story={story}
+                  user={props.user}
+                  image={props.image}
+                  replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
+                  handleDeleteStory={props.handleDeleteStory}
+                />
+              </div>
+            }}
+          />
 
-                    return <div className="StoryShow-purple">
-                      <StoryShow
-                        story={story}
-                        user={props.user}
-                        image={props.image}
-                        replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
-                        handleDeleteStory={props.handleDeleteStory}
-                      />
-                    </div>
+          <Route
+            path='/stories/:id/edit'
+            render={({match}) => {
+              const story = props.stories.find( s => s.id === parseInt(match.params.id, 10))
+              return
+              <div className="EditStoryForm-blue">
+                <EditStoryForm
+                  story={story}
+                  replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
+                  handleUpdateStory={props.handleUpdateStory}
+                  handleDeleteStory={props.handleDeleteStory}
+                />
+              </div>
+            }}
+          />
 
-                }} />
+          <Route
+            path='/stories'
+            render={() => <AllStories
+              scrollToTop={props.scrollToTop}
+              handleUpdateStory={props.handleUpdateStory}
+              handleDeleteStory={props.handleDeleteStory}
+              stories={props.stories}
+              replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
+              user={props.user}
+              // username={props.user ? props.user.name : "props.user.name here"}
+              // user_id={props.user ? props.user.id : "props.user.id here"}
+              storyShowIsModal={props.storyShowIsModal}
+              openModal={props.openModal}
+              closeModal={props.closeModal}
+              storyShowModalIsEditable={props.storyShowModalIsEditable}
+              toggleStoryShowModalToEditable={props.toggleStoryShowModalToEditable}
+              activeModalStoryId={props.activeModalStoryId}
+              indexOfStoryModal={props.indexOfStoryModal}
+            />}
+          />
 
-                <Route
-                  path='/stories/:id/edit'
-                  render={({match}) => {
-                    const story = props.stories.find( s => s.id === parseInt(match.params.id, 10))
-                    return <div className="EditStoryForm-blue">
-                      <EditStoryForm
-                        story={story}
-                        replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
-                        handleUpdateStory={props.handleUpdateStory}
-                        handleDeleteStory={props.handleDeleteStory}
-                      />
-                    </div>
-                  }} />
+          <Route
+            path='/metadata'
+            render={() => <Metadata
+              scrollToTop={props.scrollToTop}
+              stories={props.stories}
+              replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
+              plots={props.plots}
+              users={props.users}
+            />}
+          />
 
-                  <Route
-                    path='/stories'
-                    render={() => <AllStories
-                      scrollToTop={props.scrollToTop}
-                      handleUpdateStory={props.handleUpdateStory}
-                      handleDeleteStory={props.handleDeleteStory}
-                      stories={props.stories}
-                      replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
-                      user={props.user}
-                      // username={props.user ? props.user.name : "props.user.name here"}
-                      // user_id={props.user ? props.user.id : "props.user.id here"}
-                      storyShowIsModal={props.storyShowIsModal}
-                      openModal={props.openModal}
-                      closeModal={props.closeModal}
-                      storyShowModalIsEditable={props.storyShowModalIsEditable}
-                      toggleStoryShowModalToEditable={props.toggleStoryShowModalToEditable}
-                      activeModalStoryId={props.activeModalStoryId}
-                      indexOfStoryModal={props.indexOfStoryModal}
-                    />} />
-
-                    <Route
-                      path='/metadata'
-                      render={() => <Metadata
-                        scrollToTop={props.scrollToTop}
-                        stories={props.stories}
-                        replacePlotTitleWithEmoji={props.replacePlotTitleWithEmoji}
-                        plots={props.plots}
-                        users={props.users}
-                      />} />
-
-                    </Switch>
-                  </Grid.Column>
-
-
-                  <Grid.Column width={4}>
-
-                    <div className="nerdy-storypage">
-                      <img src={nerdy} className="nerdy" alt="nerdy gif"/>
-
-                      <h1 className="hoverYellow pulse-grow">Word Nerds</h1>
-                    </div>
-
-                    <div className="storypage-sentence">
-                      <Sentence />
-                    </div>
-
-                  </Grid.Column>
+        </Switch>
+      </Grid.Column>
 
 
-                {/* </Grid.Row> */}
-              </Grid>
+      <Grid.Column width={4}>
 
-            </div>
-          )
+        <div className="nerdy-storypage">
+          <img src={nerdy} className="nerdy" alt="nerdy gif"/>
 
-        } //end of StoryPage
+          <h1 className="hoverYellow pulse-grow">Word Nerds</h1>
+        </div>
 
-        //////
+        <div className="storypage-sentence">
+          <Sentence />
+        </div>
 
-        StoryPage.defaultProps = {
-          story: 'story content here', //need this so props aren't null
-          title: 'story title here',
-          // image: 'story image here',
-          genres: 'story genres here',
-          plots: 'story plots here',
-          user_id: 'user_id here',
-          stories: [
-            {
-              characters: 'story characters StoryPage defaultProps',
-              content: 'words words ----- word words words',
-              genres: [{one_genre: 'story genres here'}],
-              id: 'story ID here',
-              paragraphs: 'story paragraphs here',
-              plots: [{title: 'Halloween'}],
-              title: 'title here',
-              user: {name: 'username here'}
-            },
-          ]
-        }
+      </Grid.Column>
 
 
-        export default StoryPage
+      {/* </Grid.Row> */}
+    </Grid>
+  </div>
+)
+} //end of StoryPage
+
+//////
+
+StoryPage.defaultProps = {
+  story: 'story content here', //need this so props aren't null
+  title: 'story title here',
+  // image: 'story image here',
+  genres: 'story genres here',
+  plots: 'story plots here',
+  user_id: 'user_id here',
+  stories: [
+    {
+      characters: 'story characters StoryPage defaultProps',
+      content: 'words words ----- word words words',
+      genres: [{one_genre: 'story genres here'}],
+      id: 'story ID here',
+      paragraphs: 'story paragraphs here',
+      plots: [{title: 'Halloween'}],
+      title: 'title here',
+      user: {name: 'username here'}
+    },
+  ]
+}
+
+
+export default StoryPage
