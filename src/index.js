e4d91e794f09bb66aa7952a4c3c148004463efd4
ethanryan <ethanryan@gmail.com>
2018-07-14 22:38:44 -0400

    wrapping index in HttpsRedirect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import HttpsRedirect from 'react-https-redirect';
+
 // import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css'; //for semantic-ui-css
 import './index.css';
 import { BrowserRouter } from 'react-router-dom' //for routes
 
 ReactDOM.render((
+  <HttpsRedirect>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+</HttpsRedirect>
 ), document.getElementById('root'))
 
 // ReactDOM.render(<App />, document.getElementById('root'));
