31e3bbdef0e3103ac6eb25eebf7b228995c5514f
ethanryan <ethanryan@gmail.com>
2018-08-16 08:27:31 -0400

    reformatting line indents for CreateStoryFormNotModal

--- src/components/CreateStoryForm/CreateStoryFormNotModal.js
+++ src/components/CreateStoryForm/CreateStoryFormNotModal.js
@@ -13,87 +13,88 @@ class CreateStoryFormNotModal extends Component {
     // console.log('**** props from CreateStoryFormNotModal:', props)
     // console.log('+++++ props.genreSelection from CreateStoryFormCreateCharacters:', props.genreSelection)
     // console.log('+++++ props.characterProps from CreateStoryFormCreateCharacters:', props.characterProps)
-    this.state=({ //organizing this so it's not nested, but post request will be nested
-    // story: '',
-    characters: this.props.characterProps,
-    genreSelection: this.props.genreSelection, //chosen in CreateStoryFormSelectGenre, passed up to StoryContainer from there
-    storyType: 'story', //adding storyType...
-  })
-  this.handleCreateStoryFormSubmit = this.handleCreateStoryFormSubmit.bind(this)
-} //end of constructor
-
-
-componentWillReceiveProps(nextProps) { //need this lifecycle method to update filteredPlotsByTitle
-  // console.log('CreateStoryFormNotModal nextProps:', nextProps)
-  if (this.props !== nextProps) {
-    // console.log("CreateStoryFormNotModal - props don't equal nextProps, so updating state with nextProps: ", nextProps)
-    this.setState({
-      characters: nextProps.characterProps,
+    this.state=({
+      //organizing this so it's not nested, but post request will be nested
+      // story: '',
+      characters: this.props.characterProps,
+      genreSelection: this.props.genreSelection, //chosen in CreateStoryFormSelectGenre, passed up to StoryContainer from there
+      storyType: 'story', //adding storyType...
     })
+    this.handleCreateStoryFormSubmit = this.handleCreateStoryFormSubmit.bind(this)
+  } //end of constructor
+
+
+  componentWillReceiveProps(nextProps) { //need this lifecycle method to update filteredPlotsByTitle
+    // console.log('CreateStoryFormNotModal nextProps:', nextProps)
+    if (this.props !== nextProps) {
+      // console.log("CreateStoryFormNotModal - props don't equal nextProps, so updating state with nextProps: ", nextProps)
+      this.setState({
+        characters: nextProps.characterProps,
+      })
+    }
   }
-}
 
 
-handleCreateStoryFormSubmit(event) {
-  event.preventDefault()
-  const genres = this.props.genreSelection
-  const characters = this.state.characters
-  const user_id = this.props.user_id
-  const storyType = this.state.storyType //adding storyType...
-  // console.log('CreateStoryFormNotModal submitted this.state: ', this.state)
-  console.log('CreateStoryFormNotModal submitted:::: ', genres, characters, user_id, storyType)
-  // console.log('user_id is: ', user_id)
-  this.props.handleSubmit( genres, characters, user_id, storyType ) //adding storyType...
-  this.props.handleClearForm()
-  this.props.scrollToTop()
-}
+  handleCreateStoryFormSubmit(event) {
+    event.preventDefault()
+    const genres = this.props.genreSelection
+    const characters = this.state.characters
+    const user_id = this.props.user_id
+    const storyType = this.state.storyType //adding storyType...
+    // console.log('CreateStoryFormNotModal submitted this.state: ', this.state)
+    console.log('CreateStoryFormNotModal submitted:::: ', genres, characters, user_id, storyType)
+    // console.log('user_id is: ', user_id)
+    this.props.handleSubmit( genres, characters, user_id, storyType ) //adding storyType...
+    this.props.handleClearForm()
+    this.props.scrollToTop()
+  }
 
 
-render() {
+  render() {
 
-  return(
+    return(
 
-    <div>
-      {/* <Modal
-        className="CreateStoryFormNotModal-fix-for-now"
-        trigger={
-        <Button primary fluid>Create A Short Story</Button>
-      }> */}
+      <div>
+        {/* <Modal
+          className="CreateStoryFormNotModal-fix-for-now"
+          trigger={
+          <Button primary fluid>Create A Short Story</Button>
+        }> */}
 
-      {/* <Modal.Header> */}
+        {/* <Modal.Header> */}
         <Header as='h1' textAlign='center'>
           Create a Short Story
         </Header>
-      {/* </Modal.Header> */}
+        {/* </Modal.Header> */}
 
-      <Form onSubmit={this.handleCreateStoryFormSubmit} className="CreateStoryFormModal-red">
+        <Form onSubmit={this.handleCreateStoryFormSubmit} className="CreateStoryFormModal-red">
 
-        {/* <Modal.Content scrolling> */}
+          {/* <Modal.Content scrolling> */}
 
-        {/* <h1>user_id: {this.props.user_id}</h1> */}
+          {/* <h1>user_id: {this.props.user_id}</h1> */}
 
-        <CreateStoryFormSelectGenre
-          plots={this.props.plots}
-          handleGenreChange={this.props.handleGenreChange}
-        />
+          <CreateStoryFormSelectGenre
+            plots={this.props.plots}
+            handleGenreChange={this.props.handleGenreChange}
+          />
 
-        <CreateStoryFormCreateCharacters
-          handleCharacterNameChange={this.props.handleCharacterNameChange}
-          handleCharacterGenderChange={this.props.handleCharacterGenderChange}
-        />
+          <CreateStoryFormCreateCharacters
+            handleCharacterNameChange={this.props.handleCharacterNameChange}
+            handleCharacterGenderChange={this.props.handleCharacterGenderChange}
+          />
 
-      {/* </Modal.Content> */}
-      {/* everything within modal.content scrolls... */}
+          {/* </Modal.Content> */}
+          {/* everything within modal.content scrolls... */}
 
-      {/* <Modal.Actions> */}
-        <Form.Button content='Submit' type="submit" primary fluid/>
-      {/* </Modal.Actions> */}
+          {/* <Modal.Actions> */}
+          <Form.Button content='Submit' type="submit" primary fluid/>
+          {/* </Modal.Actions> */}
 
-      </Form>
-    {/* </Modal> */}
-  </div>
-)
-}
+        </Form>
+        {/* </Modal> */}
+      </div>
+    )
+  }
 }
 
 export default CreateStoryFormNotModal
