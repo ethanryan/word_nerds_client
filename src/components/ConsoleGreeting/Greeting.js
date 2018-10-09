bd0817883026797611ea21748eda52e3de78b0fe
ethanryan <ethanryan@gmail.com>
2018-10-08 23:16:06 -0400

    updated Greeting

--- src/components/ConsoleGreeting/Greeting.js
+++ src/components/ConsoleGreeting/Greeting.js
@@ -8,15 +8,12 @@ function getFriendlyMessage(nameString) {
     `hi there ${nameString}, you spectacular human being you!`,
     `you look awesome today ${nameString}!`,
     `hellllooooooo ${nameString}!`,
-    `Hey ${nameString}, you sexy beast!`,
+    `Hey ${nameString}, how's life?`,
     `Can you keep a secret, ${nameString}? You're my favorite!`,
     `Nothing to see here, ${nameString}.`,
-    `Hey! Get your mind out of the gutter, ${nameString}!`,
-    `My eyes are up here! jk ${nameString} i luv u`,
-    `wtf are you lookin at, ${nameString}?!`,
-    `welcome to my safe space, ${nameString}`,
+    `Congratulations, ${nameString}! You've discovered the console ;)`,
     `have i told you lately that i love you, ${nameString}?`,
-    `welcome to my world, ${nameString}...`,
+    `i knew you'd find this Easter egg eventually, ${nameString}...`,
   ]
   var randomMessage = messages[Math.floor(Math.random() * messages.length)];
   return randomMessage
@@ -25,9 +22,7 @@ function getFriendlyMessage(nameString) {
 
 class Greeting extends React.PureComponent { //PureComponent only renders once...
   render() {
-    // console.info("%cThis is green text on a yellow background.", "color:green; background-color:yellow");
-    // console.info(`%chi there ${this.props.current_user}, you spectacular human being you!`, "color:yellow; background-color:purple")
-    console.info( getFriendlyMessage(this.props.current_user) ) //not overwriting console.info...
+    console.info( getFriendlyMessage(this.props.current_user) )
     return null;
   }
 }
